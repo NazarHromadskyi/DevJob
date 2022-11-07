0850e8c9364569f279f8305b9f168291761ef229
El Padlo <nazar.hromadskyi@gmail.com>
2022-11-07 19:07:19 +0200

    Status code enum created

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,7 @@
 module.exports = {
+  emailActionEnum: require('./emailAction.enum'),
   categoryEnum: require('./category.enum'),
   levelEnum: require('./level.enum'),
   modelNames: require('./modelNames'),
+  statusCode: require('./statusCode.enum'),
 };

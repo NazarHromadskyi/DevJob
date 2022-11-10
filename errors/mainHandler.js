2b50fed2f1ac3e51654a3b083fe72b57c68e23a3
El Padlo <nazar.hromadskyi@gmail.com>
2022-11-10 14:25:56 +0200

    import fixed

--- errors/mainHandler.js
+++ errors/mainHandler.js
@@ -1,9 +1,9 @@
-const { statusCode } = require('../constants');
+const { statusCodesEnum } = require('../constants');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
   res
-    .status(err.status || statusCode.SERVER_ERROR)
+    .status(err.status || statusCodesEnum.SERVER_ERROR)
     .json({
       message: err.message || 'Server error',
     });
